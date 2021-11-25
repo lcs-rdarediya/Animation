03e2c2551d0a758bd29efbb5c65568a6cc4ddea9
Rayhan Darediya <rdarediya@lcs.on.ca>
2021-11-25 12:29:52 -0500

    finished the white circles before writing my text

--- Playground.playground/Pages/gig post remake.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/gig post remake.xcplaygroundpage/Contents.swift
@@ -57,36 +57,50 @@ canvas.drawAxes(withScale: true, by: 50, color: .black)
  [Documentation](http://russellgordon.ca/CanvasGraphics/Documentation/) is available.
  
  */
-//making a loop
+// backgorund 
 
 canvas.highPerformance = true
-for someValue in stride(from: 0, through: 600, by: 1){
-    
-    canvas.lineColor = Color(hue: 110, saturation: 80, brightness: 0, alpha: 100)
-    
-    canvas.drawLine(from: Point(x: 0, y: someValue), to: Point(x: 600, y: someValue))
-}
 
-for verticalposition in stride(from:-40, through: 400, by: 40){
-    for horizontalposition in stride(from: -40, through: 400, by: 40){
-        canvas.fillColor = Color(hue: 110, saturation: 54, brightness: 72, alpha: 100)
+
+canvas.fillColor = Color(hue: 110,
+                         saturation: 54,
+                         brightness: 72,
+                         alpha: 100)
+canvas.drawRectangle(at: Point(x: 0, y: 0), width: 400, height: 600)
+
+canvas.fillColor = Color(hue: 110, saturation: 80, brightness: 0, alpha: 100)
+canvas.drawRectangle(at: Point(x: 0, y: 0), width: 400, height: 400)
+  
+// draw the circles
+
+for verticalposition in stride(from:0, through: 400, by: 40){
+    
+    for horizontalposition in stride(from: 0, through: 400, by: 40){
+                
+        canvas.drawEllipse(at: Point(x: horizontalposition, y: verticalposition), width: 36, height: 36)
+
+        // green cicles
+        if horizontalposition == 0 ||
+            horizontalposition == 400 ||
+            verticalposition == 0 ||
+            verticalposition == 400 ||
+            //white cicles
+            horizontalposition + verticalposition > 40
+        {
+            
+            canvas.fillColor = Color(hue: 110, saturation: 54, brightness: 72, alpha: 100)
+
+        } else {
+            
+            canvas.fillColor = .white
+
+        }
         
         canvas.drawEllipse(at: Point(x: horizontalposition, y: verticalposition), width: 36, height: 36)
         
         
         
-for xValue in stride(from: 1,
-                             through: 600,
-                             by: 1){
-            
-    let currentColor = Color(hue: 110,
-                            saturation: 54,
-                            brightness: 72,
-                            alpha: 100)
-        canvas.lineColor = currentColor
-        canvas.drawLine(from: Point (x: xValue, y: 400), to: Point (x: xValue, y: 600))
-            
-}
+        
         
     }
     
